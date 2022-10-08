7df4b56b9d8f4db5ee42bf768735489cab196a26
Martin Askestad <MartinAskestad@users.noreply.github.com>
2022-10-08 06:57:44 +0200

    Add scoring

--- pong/court/court.gd
+++ pong/court/court.gd
@@ -1,17 +1,15 @@
 extends Sprite
 
-
-func _ready():
-  pass
-
-
 func _on_VisibilityNotifier2D_screen_exited():
-  if $Ball.position.x > 300:
+  if $Ball.position.x > 300:  # Player scored
     $Ball.reset(1)
-  else:
+    var score = int($PlayerScore.text) + 1
+    $PlayerScore.text = str(score)
+  else:                       # Opponent scored
     $Ball.reset(-1)
+    var score = int($OpponentScore.text) + 1
+    $OpponentScore.text = str(score)
   $Ball.position = Vector2(400, 300)
-
 
 func _on_OpponentTimer_timeout():
   $OpponentPaddle.ball_position = $Ball.position
